cc0c56e5c062e7bd6ce98123c2a0813996c09df7
castle <castlesmadeofcode@gmail.com>
2020-08-04 12:17:36 -0500

    assignment complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserOutput from './UserOutput/UserOutput'
+import UserInput from './UserInput/UserInput'
+
 
 class App extends Component {
+
+  state = {
+    username: "boopz"
+  }
+
+
+  changeUsernameHandler = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
   render() {
+
+    const style = {
+      backgroundColor: 'blue',
+    };
+
     return (
-      <div className="App">
-        <ol>
-          <li>Create TWO new components: UserInput and UserOutput</li>
-          <li>UserInput should hold an input element, UserOutput two paragraphs</li>
-          <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
-          <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
-          <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
-          <li>Add a method to manipulate the state (=> an event-handler method)</li>
-          <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
-          <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
-          <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
-          <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
-        </ol>
+      <div style={style} className="App">
+        <UserOutput paragraph1={this.paragraph1 = "Hello"}
+          paragraph2={this.paragraph2 = "World"}
+          username={this.state.username}></UserOutput>
+        <UserOutput paragraph1={this.paragraph1 = "HELLO"}
+          paragraph2={this.paragraph2 = "WORLD"} username={this.username = "boopz"}></UserOutput>
+
+        <UserInput changed={this.changeUsernameHandler}></UserInput>
       </div>
     );
   }
